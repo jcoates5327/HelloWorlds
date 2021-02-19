703a469543322d724c16b943a05e5349ab34c9cb
Justin Coates <jcoates5327@gmail.com>
2021-02-18 23:35:06 -0500

    Continuing to add more language examples.
    Some esoteric ones this time around.

--- hello_world_list.js
+++ hello_world_list.js
@@ -1,4 +1,17 @@
 const hello_worlds = {
+	"ActionScript 2": `
+<div class="code" id="actionscript 2"><p>
+createTextField("greet",&nbsp;0,&nbsp;0,&nbsp;0,&nbsp;100,&nbsp;100);<br/>
+greet.text&nbsp;=&nbsp;"Hello,&nbsp;world!";
+</p></div>
+`,
+	"ActionScript 3": `
+<div class="code" id="actionscript 3"><p>
+var&nbsp;txtHello:&nbsp;TextField&nbsp;=&nbsp;new&nbsp;TextField();<br/>
+txtHello.text&nbsp;=&nbsp;"Hello,&nbsp;world!";<br/>
+this.addChild(txtHello);
+</p></div>
+`,
 	"Ada": `
 <div class="code" id="ada"><p>
 with&nbsp;Ada.Text_IO;&nbsp;use&nbsp;Ada.Text_IO;<br/>
@@ -6,6 +19,19 @@ procedure&nbsp;Hello&nbsp;is<br/>
 begin<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;Put_Line&nbsp;("Hello,&nbsp;world!");<br/>
 end&nbsp;Hello;
+</p></div>
+`,
+	"AWK": `
+<div class="code" id="awk"><p>
+BEGIN&nbsp;{&nbsp;print&nbsp;"Hello,&nbsp;world!"&nbsp;}
+</p></div>
+`,
+	"B": `
+<div class="code" id="b"><p>
+main()&nbsp;{<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;putstr("Hello,&nbsp;world!*n");<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;return(0);<br/>
+}
 </p></div>
 `,
 	"Bash": `
@@ -25,6 +51,113 @@ echo&nbsp;"Hello,&nbsp;world!"
 @ECHO&nbsp;OFF<br/>
 ECHO&nbsp;Hello,&nbsp;world!<br/>
 PAUSE
+</p></div>
+`,
+	"Beatnik": `
+<div class="code" id="beatnik"><p>
+Soars,&nbsp;larkspurs,&nbsp;rains.<br/>
+Indistinctness.<br/>
+Mario&nbsp;snarl&nbsp;(nurses,&nbsp;natures,&nbsp;rules...)&nbsp;sensuously&nbsp;retries&nbsp;goal.<br/>
+Agribusinesses'&nbsp;costs&nbsp;par&nbsp;lain&nbsp;ropes&nbsp;(mopes)&nbsp;autos'&nbsp;cores.<br/>
+Tuner&nbsp;ambitiousness.<br/>
+Flit.<br/>
+Dour&nbsp;entombment.<br/>
+Legals'&nbsp;saner&nbsp;kinking&nbsp;lapse.<br/>
+Nests&nbsp;glint.<br/>
+Dread,&nbsp;tied&nbsp;futures,&nbsp;dourer&nbsp;usual&nbsp;tumor&nbsp;grunts&nbsp;alter&nbsp;atonal<br/>
+&nbsp;&nbsp;garb&nbsp;tries&nbsp;shouldered&nbsp;coins.<br/>
+Taste&nbsp;a&nbsp;vast&nbsp;lustiness.<br/>
+Stile&nbsp;stuns&nbsp;gad&nbsp;subgroup&nbsp;gram&nbsp;lanes.<br/>
+Draftee&nbsp;insurer&nbsp;road:&nbsp;cuckold&nbsp;blunt,&nbsp;strut&nbsp;sunnier.<br/>
+Rely&nbsp;enure&nbsp;pantheism:&nbsp;arty&nbsp;gain&nbsp;groups&nbsp;(genies,&nbsp;pan)&nbsp;titters,&nbsp;tattles,&nbsp;nears.<br/>
+Bluffer&nbsp;tapes?&nbsp;&nbsp;Idle&nbsp;diatom&nbsp;stooge!<br/>
+Feted&nbsp;antes&nbsp;anklets&nbsp;ague?&nbsp;&nbsp;Remit&nbsp;goiter&nbsp;gout!<br/>
+Doubtless&nbsp;teared&nbsp;toed&nbsp;alohas&nbsp;will&nbsp;dull&nbsp;gangs'&nbsp;aerials'&nbsp;tails'&nbsp;sluices;<br/>
+Gusset&nbsp;ends!&nbsp;&nbsp;Gawkier&nbsp;halo!<br/>
+<br/>
+Enter&nbsp;abstruse&nbsp;rested&nbsp;loser&nbsp;beer&nbsp;guy&nbsp;louts.<br/>
+Curtain&nbsp;roams&nbsp;lasso&nbsp;weir&nbsp;lupus&nbsp;stunt.<br/>
+Truant&nbsp;bears&nbsp;animate&nbsp;talon.&nbsp;&nbsp;Entire&nbsp;torte&nbsp;originally&nbsp;timer.<br/>
+Redo&nbsp;stilt&nbsp;gobs.<br/>
+<br/>
+Utter&nbsp;centaurs;<br/>
+Urgent&nbsp;stars;<br/>
+Usurers&nbsp;(dilute);<br/>
+Noses;<br/>
+Bones;<br/>
+Brig&nbsp;sonar&nbsp;graders;<br/>
+Utensil&nbsp;silts;<br/>
+Lazies.<br/>
+Fret&nbsp;arson&nbsp;veterinary&nbsp;rows.<br/>
+<br/>
+Atlas&nbsp;grunted:&nbsp;"Pates,&nbsp;slues,&nbsp;sulfuric&nbsp;manor&nbsp;liaising&nbsp;tines,<br/>
+&nbsp;&nbsp;trailers,&nbsp;rep...&nbsp;unfair!&nbsp;&nbsp;Instant&nbsp;snots!"<br/>
+<br/>
+Sled&nbsp;rested&nbsp;until&nbsp;eatery&nbsp;fail.<br/>
+Ergs&nbsp;fortitude<br/>
+&nbsp;&nbsp;Indent&nbsp;spotter<br/>
+Euros&nbsp;enter&nbsp;egg.<br/>
+Curious&nbsp;tenures.<br/>
+Torus&nbsp;cutlasses.<br/>
+Sarong&nbsp;torso&nbsp;earns&nbsp;cruel&nbsp;lags&nbsp;it&nbsp;reeled.<br/>
+<br/>
+Engineer:&nbsp;"Erase&nbsp;handbag&nbsp;--&nbsp;unite&nbsp;ratification!"<br/>
+<br/>
+oaring&nbsp;oaten&nbsp;donkeys&nbsp;unsold,&nbsp;surer&nbsp;rapid&nbsp;saltest&nbsp;tags<br/>
+BUTTERED&nbsp;TIBIA&nbsp;LUGS&nbsp;REWIRING&nbsp;TOILETS<br/>
+anion&nbsp;festers&nbsp;raring&nbsp;edit&nbsp;epilogues.<br/>
+DIRGE&nbsp;ROTOR.<br/>
+linnet&nbsp;oaring.<br/>
+GORE&nbsp;BOOTIES.<br/>
+Ironed&nbsp;goon&nbsp;lists&nbsp;tallest&nbsp;sublets&nbsp;--<br/>
+Riots,<br/>
+Raucous&nbsp;onset.<br/>
+<br/>
+Ignobly,&nbsp;runners'&nbsp;diet&nbsp;anguishes&nbsp;sunrise&nbsp;loner.<br/>
+Erode&nbsp;mob,&nbsp;slier&nbsp;switcher!<br/>
+Loaners&nbsp;stilt&nbsp;drudge&nbsp;pearl&nbsp;atoll,&nbsp;risking&nbsp;hats'&nbsp;ends.<br/>
+<br/>
+Rebind&nbsp;sitters.<br/>
+<br/>
+Toga&nbsp;epistles&nbsp;--&nbsp;crud&nbsp;lard.&nbsp;&nbsp;(Pager&nbsp;purse&nbsp;dons&nbsp;souls.)<br/>
+<br/>
+glob&nbsp;title&nbsp;a&nbsp;curio&nbsp;hired&nbsp;rites&nbsp;shed&nbsp;suds&nbsp;lade&nbsp;grease&nbsp;strut&nbsp;arctic&nbsp;revs&nbsp;toad<br/>
+unless&nbsp;idlers&nbsp;rind&nbsp;stilt&nbsp;region&nbsp;land&nbsp;GERMICIDES&nbsp;SULTANA&nbsp;GUTS&nbsp;gill&nbsp;siting&nbsp;leans<br/>
+nice&nbsp;spurs<br/>
+tests&nbsp;gloves<br/>
+roused&nbsp;asp<br/>
+<br/>
+Holes!&nbsp;&nbsp;Moles!&nbsp;&nbsp;(Sores!)<br/>
+Hygienists!&nbsp;&nbsp;Scars!&nbsp;&nbsp;(Asses!)<br/>
+Smells&nbsp;spell&nbsp;rares.<br/>
+<br/>
+Cubs&nbsp;instant&nbsp;sing&nbsp;in&nbsp;parse&nbsp;goodies.<br/>
+Rosin.&nbsp;&nbsp;Unhelpful&nbsp;sisal&nbsp;acres.&nbsp;&nbsp;Slope&nbsp;told.<br/>
+MALENESS&nbsp;PASTA&nbsp;LAB.&nbsp;&nbsp;"Infirmary&nbsp;vine,"&nbsp;rang&nbsp;illiterates&nbsp;(beans).<br/>
+Rosin&nbsp;sours,&nbsp;insults&nbsp;truss&nbsp;abalones,&nbsp;nailed&nbsp;rules,&nbsp;helical&nbsp;atlases.<br/>
+Dear&nbsp;remodeling&nbsp;stings&nbsp;mar&nbsp;rents.<br/>
+Sunless&nbsp;shiner&nbsp;orb&nbsp;(silly&nbsp;idol.)<br/>
+Clarity&nbsp;disses&nbsp;senna.<br/>
+Vagabonds&nbsp;sauted;&nbsp;sloes&nbsp;performed&nbsp;gelds.<br/>
+Alter&nbsp;post&nbsp;radial&nbsp;lip&nbsp;sectioning&nbsp;gums.<br/>
+Saint&nbsp;Towellings.<br/>
+Larger&nbsp;aeons&nbsp;telephone&nbsp;stolid&nbsp;char,&nbsp;pal!<br/>
+Boats&nbsp;Dean&nbsp;forsook,&nbsp;rosters,&nbsp;tunas,&nbsp;terrariums&nbsp;--&nbsp;united,&nbsp;traced.<br/>
+Nude&nbsp;pagoda&nbsp;careens.
+</p></div>
+`,
+	"Befunge": `
+<div class="code" id="befunge"><p>
+>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;v<br/>
+v&nbsp;&nbsp;,,,,,"Hello,"<<br/>
+>48*,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;v<br/>
+v,,,,,,"world!"&nbsp;<<br/>
+>25*,@
+</p></div>
+`,
+	"Brainfuck": `
+<div class="code" id="brainfuck"><p>
+++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
 </p></div>
 `,
 	"C#": `
@@ -56,6 +189,29 @@ int&nbsp;main(void)&nbsp;{<br/>
 }
 </p></div>
 `,
+	"Chef": `
+<div class="code" id="chef"><p>
+Hello&nbsp;World&nbsp;Souffle.<br/>
+<br/>
+This&nbsp;recipe&nbsp;prints&nbsp;the&nbsp;immortal&nbsp;words&nbsp;"Hello,&nbsp;world!",&nbsp;in&nbsp;a&nbsp;basically&nbsp;brute&nbsp;force&nbsp;way.&nbsp;It&nbsp;also&nbsp;makes&nbsp;a&nbsp;lot&nbsp;of&nbsp;food&nbsp;for&nbsp;one&nbsp;person.<br/>
+<br/>
+Ingredients.<br/>
+72&nbsp;g&nbsp;haricot&nbsp;beans<br/>
+101&nbsp;eggs<br/>
+108&nbsp;g&nbsp;lard<br/>
+111&nbsp;cups&nbsp;oil<br/>
+32&nbsp;zucchinis<br/>
+119&nbsp;ml&nbsp;water<br/>
+114&nbsp;g&nbsp;red&nbsp;salmon<br/>
+100&nbsp;g&nbsp;dijon&nbsp;mustard<br/>
+33&nbsp;potatoes<br/>
+<br/>
+Method.<br/>
+Put&nbsp;potatoes&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;dijon&nbsp;mustard&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;lard&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;red&nbsp;salmon&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;oil&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;water&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;zucchinis&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;oil&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;lard&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;lard&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;eggs&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Put&nbsp;haricot&nbsp;beans&nbsp;into&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Liquefy&nbsp;contents&nbsp;of&nbsp;the&nbsp;mixing&nbsp;bowl.&nbsp;Pour&nbsp;contents&nbsp;of&nbsp;the&nbsp;mixing&nbsp;bowl&nbsp;into&nbsp;the&nbsp;baking&nbsp;dish.<br/>
+<br/>
+Serves&nbsp;1.
+</p></div>
+`,
 	"Clojure": `
 <div class="code" id="clojure"><p>
 (println&nbsp;"Hello,&nbsp;world!")
@@ -81,6 +237,13 @@ import&nbsp;std.stdio;<br/>
 <br/>
 void&nbsp;main()&nbsp;{<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;writeln("Hello,&nbsp;world!");<br/>
+}
+</p></div>
+`,
+	"Dart": `
+<div class="code" id="dart"><p>
+void&nbsp;main()&nbsp;{<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;print('Hello,&nbsp;world!');<br/>
 }
 </p></div>
 `,
@@ -136,6 +299,26 @@ main&nbsp;::&nbsp;IO&nbsp;()<br/>
 main&nbsp;=&nbsp;putStrLn&nbsp;"Hello,&nbsp;world!"
 </p></div>
 `,
+	"INTERCAL": `
+<div class="code" id="intercal"><p>
+DO&nbsp;,1&nbsp;<-&nbsp;#13<br/>
+PLEASE&nbsp;DO&nbsp;,1&nbsp;SUB&nbsp;#1&nbsp;<-&nbsp;#238<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#2&nbsp;<-&nbsp;#108<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#3&nbsp;<-&nbsp;#112<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#4&nbsp;<-&nbsp;#0<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#5&nbsp;<-&nbsp;#64<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#6&nbsp;<-&nbsp;#194<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#7&nbsp;<-&nbsp;#48<br/>
+PLEASE&nbsp;DO&nbsp;,1&nbsp;SUB&nbsp;#8&nbsp;<-&nbsp;#22<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#9&nbsp;<-&nbsp;#248<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#10&nbsp;<-&nbsp;#168<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#11&nbsp;<-&nbsp;#24<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#12&nbsp;<-&nbsp;#16<br/>
+DO&nbsp;,1&nbsp;SUB&nbsp;#13&nbsp;<-&nbsp;#162<br/>
+PLEASE&nbsp;READ&nbsp;OUT&nbsp;,1<br/>
+PLEASE&nbsp;GIVE&nbsp;UP
+</p></div>
+`,
 	"Java": `
 <div class="code" id="java"><p>
 public&nbsp;class&nbsp;HelloWorld&nbsp;{<br/>
@@ -155,6 +338,24 @@ console.log("Hello,&nbsp;world!");
 fun&nbsp;main()&nbsp;{<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;println("Hello,&nbsp;world!")<br/>
 }
+</p></div>
+`,
+	"LOLCODE": `
+<div class="code" id="lolcode"><p>
+HAI&nbsp;1.2<br/>
+CAN&nbsp;HAS&nbsp;STDIO?<br/>
+VISIBLE&nbsp;"Hello,&nbsp;world!"<br/>
+KTHXBYE
+</p></div>
+`,
+	"Lua": `
+<div class="code" id="lua"><p>
+print("Hello,&nbsp;world!")
+</p></div>
+`,
+	"Malbolge": `
+<div class="code" id="malbolge"><p>
+(=<`#9]~6ZY32Vx/4Rs+0No-&Jk)"Fh}|Bcy?`=*z]Kw%oG4UUS0/@-ejc(:'8dc
 </p></div>
 `,
 	"MATLAB": `
@@ -199,6 +400,11 @@ print("Hello,&nbsp;world!\n");
 &nbsp;&nbsp;&nbsp;&nbsp;echo&nbsp;"Hello,&nbsp;world!"&nbsp;.&nbsp;PHP_EOL;
 </p></div>
 `,
+	"Pico": `
+<div class="code" id="pico"><p>
+display('Hello,&nbsp;world!',&nbsp;eoln)
+</p></div>
+`,
 	"Powershell": `
 <div class="code" id="powershell"><p>
 Write-Host&nbsp;"Hello,&nbsp;world!"
@@ -220,6 +426,12 @@ print("Hello,&nbsp;world!")
 	"R": `
 <div class="code" id="r"><p>
 print("Hello,&nbsp;world!")
+</p></div>
+`,
+	"Racket": `
+<div class="code" id="racket"><p>
+#lang&nbsp;racket/base<br/>
+"Hello,&nbsp;world!"
 </p></div>
 `,
 	"Ruby": `
@@ -268,11 +480,54 @@ import&nbsp;Swift<br/>
 print("Hello,&nbsp;world!")
 </p></div>
 `,
+	"Thue": `
+<div class="code" id="thue"><p>
+a::=~Hello,&nbsp;world!<br/>
+::=<br/>
+a
+</p></div>
+`,
+	"TypeScript": `
+<div class="code" id="typescript"><p>
+alert('Hello,&nbsp;world!');
+</p></div>
+`,
 	"Visual Basic": `
 <div class="code" id="visual basic"><p>
 Private&nbsp;Sub&nbsp;Form_Load()<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;MsgBox&nbsp;"Hello,&nbsp;world!"<br/>
 End&nbsp;Sub
+</p></div>
+`,
+	"Whitespace": `
+<div class="code" id="whitespace"><p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+&nbsp;&nbsp;<br/>
+<br/>
+<br/>
+
 </p></div>
 `,
 	"x86 Assembly": `
